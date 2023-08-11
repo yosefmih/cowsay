8690c3a80325f5174f86f75d651ce839f3383225
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-08-11 14:46:00 -0400

    feat: use as catch-all

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run()
