251b4c1fd270e7448df2b04e25c6ac3314e71af1
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-08-14 14:42:29 -0400

    feat: use an f-string to show the path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
-    return "<p>Hello, World!</p>"
+    return f"<p>Hello, World! Welcome to {path}</p>"
 
 if __name__ == '__main__':
     app.run()
