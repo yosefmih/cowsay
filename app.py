5ad79967cc98bda829aab83f46b8eb7b02a13552
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-08-17 11:39:00 -0400

    feat: give more variation to memory consumption

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
-    some_str = ' ' * (1000000 * random.randint(1, 16))
+    rand_one = random.randint(1, 16)
+    rand_two = random.randint(1, 16)
+    some_str = ' ' * (1000000 * (rand_one + rand_two))
     return f"<p>Hello, World! Welcome to {path}</p>"
 
 if __name__ == '__main__':
