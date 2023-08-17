d052205b4785a8306bd782a78065b084e7c0e502
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-08-17 10:50:07 -0400

    feat: consume a random amount of memory

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_world(path):
+    some_str = ' ' * (1000000 * random.randint(4, 32))
     return f"<p>Hello, World! Welcome to {path}</p>"
 
 if __name__ == '__main__':
